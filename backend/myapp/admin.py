4a33bb6ab20f9517268f16cba9ffc748d60095f9
Anita Waiba <np03cs4a220331@heraldcollege.edu.np>
2025-03-13 12:00:00 +0545

    Worked on admin

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Lesson
 
-# Register your models here.
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
